6f2aa730b8b18175b81145eaec6d7957ad7eec3e
specialkkkk <121652193+specialkkkk@users.noreply.github.com>
2023-02-26 20:06:09 +0900

    2023 02 06

--- testServlet/src/main/java/com/person/PersonServlet.java
+++ testServlet/src/main/java/com/person/PersonServlet.java
@@ -9,54 +9,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/PersonServlet")
+//@WebServlet("/PersonServlet")
 public class PersonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-	
+     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//데이터
-		String name = request.getParameter("name");
-		String gender = request.getParameter("gender");
-		String color = request.getParameter("color");
-		
-		
-		//응답
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body style='background: yellow;'>");
-		out.println("<ul style='border: 1px solid red;>");
-		out.println("<li>이  름 : " + name);
-		out.println("<br>");
-		
-		out.println("<li>성  별 : " + gender);
-		if(gender.equals("0")) out.print("남");
-		else if(gender.equals("1")) out.println("여");
-		
-		out.println("<li>색  깔 : " + color);
-		out.println("<br>");
-		
-		out.println("<li>취  미 : ");
-		for(int i=0; i<hobby.length; i++) {
-			out.println(hobby[i]);
+	// data	
+	String name = request.getParameter("name");
+	int gender = Integer.parseInt(request.getParameter("gender"));
+	String color = request.getParameter("color");
+	String[] hobbies = request.getParameterValues("hobby");
+	String[] subjects = request.getParameterValues("subject");
+	
+	// response
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();//브라우저로 나올수있게 해준다.
+	out.println("<html>");
+	out.println("<body style='background: yellow;'>");
+	out.println("<ul style='border: 1px solid red; color: "+color+"'>");
+	out.println("<li>이름 : " + name + "</li><br>");
+	if (gender == 0) out.println("<li>성별 : 남자" + "</li><br>");
+	else out.println("<li>성별 : 여자" + "</li><br>");
+	out.println("<li>색깔 : " + color + "</li><br>");
+	out.println("<li>취미 :  ");
+	if (hobbies != null) {
+		for(int i=0; i<hobbies.length; i++) {
+			out.print(hobbies[i]+" ");
 		}
-		
-		out.println("<li>과 목 : ");
-		for(String data : subject ) {
-				out.println(data);
+	}
+	out.println("<li>과목 :  ");
+	if (subjects != null) {
+		for(int i=0; i<subjects.length; i++) {
+			out.print(subjects[i]+" ");
 		}
-		
-			
-		
-		out.println("</ul>");
-		out.println("</body");
-		out.println("</html");
-		
-		
-		
+	}
+	  
+
+	out.println("</ul>");
+	out.println("</body>");
+	out.println("</html>");
 	}
 
 }
